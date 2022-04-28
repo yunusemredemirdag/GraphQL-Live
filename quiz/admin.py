601e236143a1ed5b9ba428e7dbbaf147bacf957c
Yunus Emre <yunus_kole2323@outlook.com>
2022-04-28 13:58:13 +0300

    Added GraphQL CRUD Operations

--- quiz/admin.py
+++ quiz/admin.py
@@ -4,6 +4,7 @@ from . import models
 @admin.register(models.Category)
 class CatAdmin(admin.ModelAdmin):
 	list_display = [
+        'id',
         'name',
         ]
 
--- quiz/schema.py
+++ quiz/schema.py
@@ -1,9 +1,11 @@
+from unicodedata import category
 import graphene
 from graphene_django import DjangoObjectType, DjangoListField
+from pkg_resources import require
 from .models import Quizzes, Category, Question, Answer
 
 
-class Category(DjangoObjectType):
+class CategoryType(DjangoObjectType):
     class Meta:
         model = Category
         fields = ('id', 'name')
@@ -38,5 +40,36 @@ class Query(graphene.ObjectType):
     def resolve_all_answers(root, info, id):
         return Question.objects.filter(question=id)
     
+    
+# class CategoryMutation(graphene.Mutation):
+    
+#     class Arguments:
+#         name = graphene.String(required=True)
+        
+#     category = graphene.Field(CategoryType)
+    
+#     @classmethod
+#     def mutate(cls, root, info, name):
+#         category = Category(name=name)
+#         category.save()
+#         return CategoryMutation(category=category)
 
-schema = graphene.Schema(query=Query)
+
+class CategoryMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+    
+    category = graphene.Field(CategoryType)
+    
+    @classmethod
+    def mutate(cls, root, info, id):
+        category = Category.objects.get(id=id)
+        category.delete()
+        return
+
+
+class Mutation(graphene.ObjectType):
+    update_category = CategoryMutation.Field()
+    
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
