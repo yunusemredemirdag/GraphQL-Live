e981c08d683d97c4215c33422d793e756bb42383
Yunus Emre <yunus_kole2323@outlook.com>
2022-04-28 13:27:57 +0300

    Added GraphQL Queries

---
+++ quiz/schema.py
@@ -0,0 +1,42 @@
+import graphene
+from graphene_django import DjangoObjectType, DjangoListField
+from .models import Quizzes, Category, Question, Answer
+
+
+class Category(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
+        
+    
+class QuizzesType(DjangoObjectType):
+    class Meta:
+        model = Quizzes
+        fields = ('id', 'title', 'category', 'quiz')
+        
+    
+class QuestionType(DjangoObjectType):
+    class Meta:
+        model = Question
+        fields = ('title', 'quiz')
+
+
+class AnswerType(DjangoObjectType):
+    class Meta:
+        model = Answer
+        fields = ('question', 'answer_text')
+        
+
+class Query(graphene.ObjectType):
+    
+    all_questions = graphene.Field(QuestionType, id=graphene.Int())
+    all_answers = graphene.Field(AnswerType, id=graphene.Int())
+    
+    def resolve_all_questions(root, info, id):
+        return Question.objects.get(pk=id)
+    
+    def resolve_all_answers(root, info, id):
+        return Question.objects.filter(question=id)
+    
+
+schema = graphene.Schema(query=Query)
